d7c33c35f21084887f32bd45f8c262d91f97cfa6
Nico Chan <parcseechan@yahoo.com>
2019-03-19 21:49:54 -0700

    Integrated fingerprint signup n login

---
+++ alarms/app/src/main/java/c/cpen391/alarms/CustomSharedPreference.java
@@ -0,0 +1,30 @@
+package c.cpen391.alarms;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class CustomSharedPreference {
+
+    private SharedPreferences sharedPref;
+
+    public CustomSharedPreference(Context context) {
+        sharedPref = context.getSharedPreferences("SHARED_PREF", Context.MODE_PRIVATE);
+    }
+
+    public SharedPreferences getInstanceOfSharedPreference()
+    {
+        return sharedPref;
+    }
+
+    //Save user information
+    public void setUserData(String userData)
+    {
+        sharedPref.edit().putString("USER", userData).apply();
+    }
+
+    public String getUserData()
+    {
+        return sharedPref.getString("USER", "");
+    }
+
+}
