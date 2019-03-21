12a98826b627d087d79923fae6de34060c68d2cc
Amelia Casciola <amelia.casciola@gmail.com>
2019-03-21 13:48:35 -0700

    dumb game

---
+++ alarms/app/src/main/java/c/cpen391/alarms/Circle.java
@@ -0,0 +1,18 @@
+package c.cpen391.alarms;
+
+public class Circle {
+    public int x;
+    public int y;
+    public int radius;
+    public int color;
+
+    public Circle(int x, int y, int color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.radius = 20;
+    }
+    public void incrRadius() {
+        this.radius += 20;
+    }
+}
