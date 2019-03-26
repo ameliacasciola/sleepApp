c6c093e8af0f4f8184356a07ea2ee862d22a9280
Nico Chan <parcseechan@yahoo.com>
2019-03-25 23:28:29 -0700

    profile pic n logout done

--- alarms/app/src/main/java/c/cpen391/alarms/models/UserObject.java
+++ alarms/app/src/main/java/c/cpen391/alarms/models/UserObject.java
@@ -1,14 +1,14 @@
 package c.cpen391.alarms.models;
 
 
+import android.net.Uri;
+
 public class UserObject {
 
     private String username;
     private String email;
     private String password;
-    private String address;
-    private String phone;
-    private boolean loginOption;
+    private volatile Uri uri;
 
     public UserObject(String username, String email, String password) {
         this.username = username;
@@ -27,6 +27,12 @@ public class UserObject {
     public String getPassword()
     {
         return password;
+    }
+    public Uri getUri() {
+        return uri;
+    }
+    public void putUri(Uri i) {
+        this.uri = i;
     }
 
 }
