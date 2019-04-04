95cbbf65cb18f1bf99d40f07939991670f1eb6b7
Amelia Casciola <amelia.casciola@gmail.com>
2019-04-03 19:31:12 -0700

    fixed back button

--- alarms/app/src/main/java/c/cpen391/alarms/games/GraphicsActivity.java
+++ alarms/app/src/main/java/c/cpen391/alarms/games/GraphicsActivity.java
@@ -18,6 +18,8 @@ import com.spotify.android.appremote.api.SpotifyAppRemote;
 import c.cpen391.alarms.R;
 import c.cpen391.alarms.home;
 
+import static c.cpen391.alarms.games.GraphicsView.timerDone;
+
 public class GraphicsActivity extends AppCompatActivity {
     private Context context = this;
     private Button goHome;
@@ -52,45 +54,51 @@ public class GraphicsActivity extends AppCompatActivity {
         }
 
         goHome = (Button) findViewById(R.id.home_button);
+        //goHome.setVisibility(View.GONE);
 
         //set button functionality to take you back to the tip exercise
         goHome.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                if(timerDone) {
 
-                final String CLIENT_ID = "e1cac6772536416882b7ee89591095ea";
-                final String REDIRECT_URI = "http://localhost:8000/callback/";
+                    final String CLIENT_ID = "e1cac6772536416882b7ee89591095ea";
+                    final String REDIRECT_URI = "http://localhost:8000/callback/";
 
-                ConnectionParams connectionParams =
-                        new ConnectionParams.Builder(CLIENT_ID)
-                                .setRedirectUri(REDIRECT_URI)
-                                .showAuthView(true)
-                                .build();
+                    ConnectionParams connectionParams =
+                            new ConnectionParams.Builder(CLIENT_ID)
+                                    .setRedirectUri(REDIRECT_URI)
+                                    .showAuthView(true)
+                                    .build();
 
-                SpotifyAppRemote.connect(context, connectionParams,
-                        new Connector.ConnectionListener() {
+                    SpotifyAppRemote.connect(context, connectionParams,
+                            new Connector.ConnectionListener() {
 
-                            SpotifyAppRemote mSpotifyAppRemote;
+                                SpotifyAppRemote mSpotifyAppRemote;
 
-                            @Override
-                            public void onConnected(SpotifyAppRemote spotifyAppRemote) {
-                                mSpotifyAppRemote = spotifyAppRemote;
-                                Log.e("SPOTIFY REMOTE", "Success, Onconnected" + mSpotifyAppRemote.isConnected());
+                                @Override
+                                public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                                    mSpotifyAppRemote = spotifyAppRemote;
+                                    Log.e("SPOTIFY REMOTE", "Success, Onconnected" + mSpotifyAppRemote.isConnected());
 
-                                // Now you can start interacting with App Remote
-                                mSpotifyAppRemote.getPlayerApi().pause();
-                            }
+                                    // Now you can start interacting with App Remote
+                                    mSpotifyAppRemote.getPlayerApi().pause();
+                                }
 
-                            @Override
-                            public void onFailure(Throwable throwable) {
-                                Log.e("SPOTIFY REMOTE", "Failure, Onconnected");
+                                @Override
+                                public void onFailure(Throwable throwable) {
+                                    Log.e("SPOTIFY REMOTE", "Failure, Onconnected");
 
-                                // Something went wrong when attempting to connect! Handle errors here
-                            }
-                        });
+                                    // Something went wrong when attempting to connect! Handle errors here
+                                }
+                            });
 
-                completed = true;
-                Intent intent = new Intent(context, home.class);
-                context.startActivity(intent);
+                    completed = true;
+                    Intent intent = new Intent(context, home.class);
+                    context.startActivity(intent);
+                }
+                else {
+                    Toast.makeText(context.getApplicationContext(), "Complete the game to stop the alarm!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
